fd53413cb74a3a39fc4922ee71b314966c79ee1b
jsyjst <y8588130@163.com>
2019-05-21 18:11:34 +0800

    add easy 028

---
+++ src/easy/_028/Solution.java
@@ -0,0 +1,50 @@
+package easy._028;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <pre>
+ *     author : 残渊
+ *     time   : 2019/05/21
+ *     desc   :实现 strStr() 函数。
+ *             给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。
+ *             如果不存在，则返回  -1。
+ *             说明:当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ *             对于本题而言，当 needle 是空字符串时我们应当返回 0 。
+ *             这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ * </pre>
+ */
+public class Solution {
+
+    public static void main(String[] args){
+        System.out.println(strStr("mississippi","issip"));
+    }
+    /**
+     * 示例 1:
+     * 输入: haystack = "hello", needle = "ll"
+     * 输出: 2
+     *
+     * 示例 2:
+     * 输入: haystack = "aaaaa", needle = "bba"
+     * 输出: -1
+     *
+     */
+    public static int strStr(String haystack, String needle) {
+        if(haystack.length()==0&&needle.length()==0) return 0;
+        if(haystack.length()<needle.length()) return -1;
+        int h=0,n=0;
+        while (h<haystack.length()-1){
+            if(haystack.charAt(h++)== needle.charAt(n)){
+                n++;
+                if(n==needle.length()){
+                    return h-n;
+                }
+            }else {
+                h-=n;//回到匹配字符串的下一个
+                n=0;
+            }
+        }
+        return -1;
+    }
+}
