6233e0f8019c939740c900c60fd78e65b0727bab
jsyjst <y8588130@163.com>
2019-05-27 13:48:17 +0800

    add medium 031

--- src/medium/_031/Solution.java
+++ src/medium/_031/Solution.java
@@ -1,5 +1,7 @@
 package medium._031;
 
+import java.util.Arrays;
+
 /**
  * <pre>
  *     author : 残渊
@@ -10,6 +12,15 @@ package medium._031;
  * </pre>
  */
 public class Solution {
+
+    public static void main(String[] args){
+        int nums[] = {1,2,5,3,2,1};
+        Arrays.sort(nums,3,nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+    }
     /**
      *以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
      * 1,2,3 → 1,3,2
@@ -18,6 +29,36 @@ public class Solution {
      */
 
     public void nextPermutation(int[] nums) {
-
+        int target; //记录更换的位序
+        int i = nums.length-1;
+        while (i>0){
+            if(nums[i]>nums[i-1]){
+                //找到刚好比目标值大的下标
+                target = i-1;
+                i++;
+                while (i<nums.length&&nums[target]<nums[i]){//从该下标向右移动，找到刚好比目标值大的下标
+                    i++;
+                }
+                swap(nums,target,i-1);
+                Arrays.sort(nums,target+1,nums.length);//从小到大排列交换后的数字
+                return;
+            }else {//从后面向左移动
+                i--;
+            }
+        }
+        Arrays.sort(nums);
     }
+
+    /**
+     * 交换
+     * @param nums 数组
+     * @param i 需要交换的靠近左边的数
+     * @param j 需要交换的靠近右边的数
+     */
+    public static void swap(int[] nums,int i,int j){
+        int k = nums[j];
+        nums[j] = nums[i];
+        nums[i] = k;
+    }
+
 }
