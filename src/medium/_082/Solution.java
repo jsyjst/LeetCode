3ba20d9e2d893ff75861d1dfe90498a5219e8f40
jsyjst <y8588130@163.com>
2019-06-24 21:02:19 +0800

    add medium 82

--- src/medium/_082/Solution.java
+++ src/medium/_082/Solution.java
@@ -19,11 +19,49 @@ public class Solution {
      */
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null||head.next == null) return head;
-        ListNode p = new ListNode(-1);
-        p.next = head;
-        boolean isHead = false;
-        while (p.next!=null){
-            
+        ListNode head1 = new ListNode(-1);
+        ListNode p= head.next,p1= head,p2 = head1;
+        if(p.val!=head.val) {
+            p2.next = head;
+            p2 = p2.next;
         }
+        while (p.next!=null){
+            if(p.val != p1.val&&p.val!=p.next.val){
+                p1 = p;
+                p2.next = p;
+                p2 = p2.next;
+            }else if(p.val == p.next.val){
+                p1 = p;
+            }
+            p = p.next;
+        }
+        if(p.val!= p1.val){
+            p2.next = p;
+            p2 = p2.next;
+        }
+        p2.next = null;
+        return head1.next;
+    }
+
+    /**
+     * 优化
+     *
+     */
+    public ListNode deleteDuplicatesBetter(ListNode head) {
+        if(head == null||head.next == null) return head;
+        ListNode headNew = new ListNode(-1);
+        headNew.next = head;
+        ListNode fast = headNew.next;
+        ListNode slow = headNew;
+        while (fast!=null){
+            while (fast.next!=null&&fast.val == fast.next.val) fast = fast.next;
+            if(slow.next == fast){
+                slow = slow.next;
+            }else {
+                slow.next = fast.next;
+            }
+            fast = fast.next;
+        }
+        return headNew.next;
     }
 }
