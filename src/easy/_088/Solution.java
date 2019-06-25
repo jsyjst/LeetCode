6f2b2e1510b8be5213d763c85e4e2c0c8d855391
jsyjst <y8588130@163.com>
2019-06-25 19:12:16 +0800

    add easy 88

---
+++ src/easy/_088/Solution.java
@@ -0,0 +1,44 @@
+package easy._088;
+
+import java.util.Arrays;
+
+/**
+ * <pre>
+ *     author : 残渊
+ *     time   : 2019/06/25
+ *     desc   : 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ *
+ *              说明:初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ *              你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ * </pre>
+ */
+public class Solution {
+    /**
+     * 输入:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     *
+     * 输出: [1,2,2,3,5,6]
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums2.length == 0) return;
+        for (int i = 0; i < m+n ; i++) {
+            nums1[i+m] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+
+    /**
+     * 优化
+     */
+    public void mergeBetter(int[] nums1, int m, int[] nums2, int n) {
+        int pos = m+n-1;
+        while (n >0){
+            if(m>0&&nums1[m-1]>=nums2[n-1]){
+                nums1[pos--] = nums1[--m];
+            }else{
+                nums1[pos--] = nums2[--n];
+            }
+        }
+    }
+}
