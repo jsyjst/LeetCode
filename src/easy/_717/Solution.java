74c901ab839ec4b78e13393302ddd1ef3e65b7a5
jsyjst <y8588130@163.com>
2019-11-28 20:02:27 +0800

    add 717,720

---
+++ src/easy/_717/Solution.java
@@ -0,0 +1,11 @@
+package easy._717;
+
+/**
+ * <pre>
+ *     author : jsyjst
+ *     time   : 2019/11/28
+ *     desc   :
+ * </pre>
+ */
+public class Solution {
+}
---
+++ src/easy/_720/Solution.java
@@ -0,0 +1,11 @@
+package easy._720;
+
+/**
+ * <pre>
+ *     author : jsyjst
+ *     time   : 2019/11/28
+ *     desc   :
+ * </pre>
+ */
+public class Splution {
+}
