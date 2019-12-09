9c3598031d3691e9c451bfa7f17852064ff38c61
jsyjst <y8588130@163.com>
2019-12-09 22:12:26 +0800

    update 63

--- src/medium/_063/Solution.java
+++ src/medium/_063/Solution.java
@@ -12,6 +12,13 @@ package medium._063;
  * </pre>
  */
 public class Solution {
+
+    public static void main(String[] args){
+        int[][] nums = {
+                {0,1}
+        };
+        System.out.println(uniquePathsWithObstaclesBetter(nums));
+    }
     /**
      * 示例 1:
      * 输入:
@@ -52,5 +59,29 @@ public class Solution {
             }
         }
         return paths[n-1][m-1];
+    }
+
+    public static int uniquePathsWithObstaclesBetter(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        if(m == 0) return 0;
+        int n = obstacleGrid[0].length;
+        if(n == 0) return 0;
+        int[] dp = new int[n];
+        if(obstacleGrid[0][0] == 1) return 0;
+        for(int i = 0; i < n; i++)
+            if(obstacleGrid[0][i] == 0)
+                dp[i] = 1;
+            else
+                break;
+        for(int i = 1; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[j] = 0;
+                }else if(j != 0)  {
+                    dp[j] += dp[j-1];
+                }
+            }
+        }
+        return dp[n-1];
     }
 }
