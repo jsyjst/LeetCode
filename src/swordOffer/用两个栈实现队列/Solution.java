fe6983a5f720f062ad0c1a3e46faa270957e9754
jsyjst <y8588130@163.com>
2019-12-11 21:53:12 +0800

    剑指offer:栈实现队列

---
+++ src/swordOffer/用两个栈实现队列/Solution.java
@@ -0,0 +1,29 @@
+package swordOffer.用两个栈实现队列;
+
+import java.util.Stack;
+
+/**
+ * <pre>
+ *     author : jsyjst
+ *     time   : 2019/12/11
+ *     desc   : 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * </pre>
+ */
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
+}
