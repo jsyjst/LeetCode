8222ed4ac60af2dcc29a363afaedcba20df06dec
jsyjst <y8588130@163.com>
2020-02-24 23:45:01 +0800

    add 006 to 数组

---
+++ src/排序算法/冒泡/Solution.java
@@ -0,0 +1,11 @@
+package 排序算法.冒泡;
+
+/**
+ * <pre>
+ *     author : jsyjst
+ *     time   : 2020/02/24
+ *     desc   :
+ * </pre>
+ */
+public class Solution {
+}
---
+++ src/数组/medium/_006/Solution.java
@@ -0,0 +1,11 @@
+package 数组.medium._006;
+
+/**
+ * <pre>
+ *     author : jsyjst
+ *     time   : 2020/02/24
+ *     desc   :
+ * </pre>
+ */
+public class Solution {
+}
