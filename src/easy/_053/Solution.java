03a5628e6ca19b7fce2ce6f25af2d7d218daebf5
jaceyuan <jaceyuan@tencent.com>
2022-07-12 00:02:55 +0800

    feat: 增加53动态规划解法

--- src/easy/_053/Solution.java
+++ src/easy/_053/Solution.java
@@ -4,7 +4,12 @@ package easy._053;
  * <pre>
  *     author : jsyjst
  *     time   : 2019/12/08
- *     desc   :
+ *     desc   : 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *     子数组 是数组中的一个连续部分。
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
  * </pre>
  */
 public class Solution {
@@ -23,6 +28,20 @@ public class Solution {
                 sub += nums[i];
                 res = Math.max(sub,res);
             }
+        }
+        return res;
+    }
+
+    /**
+     * 动态规划求解
+     */
+    public int maxSubArrayByDp(int[] nums) {
+        int len = nums.length;
+        int pre = nums[0];
+        int res = pre;
+        for(int i = 1; i < len; i++) {
+            pre = Math.max(pre + nums[i], nums[i]);
+            res = Math.max(pre, res);
         }
         return res;
     }
